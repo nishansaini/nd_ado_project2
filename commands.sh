c6332920b77eafd1c8e1181f2d72f82a4a721adb
nishansaini <nishan.saini@gmail.com>
2021-04-25 17:55:23 -0400

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,1 +1,3 @@
-az webapp up --sku F1 --name flask-ml-lingchenzhu --location "East US"
+az webapp up --sku F1 --name flask-ml-nishan --location "East US2"
+
+az webapp log tail --ids /subscriptions/3f968a33-f62f-4ca7-a1f1-961635306fc2/resourceGroups/testnishan1_rg_Linux_eastus2/providers/Microsoft.Web/sites/flask-ml-nishan
